411e3333dbf1211d7717593c13e9681a8d9650db
ashrafali2000 <ali4282271@gmail.com>
2024-03-18 14:04:31 +0500

    create project and install libs

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const speechRoutes = require("./routes/speech.js");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 8000;
+
+app.use("/speech", speechRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome, your app is working well");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ routes/speech.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const PlayHT = require("playht");
+const router = express.Router();
+require("dotenv").config();
+router.post("/", async (req, res) => {
+  const { userInput } = req.body;
+  console.log(userInput);
+  try {
+    PlayHT.init({
+      apiKey: process.env.PLAYCHAT_API_KEY,
+      userId: process.env.PLAYCHAT_USER_ID,
+      defaultVoiceId:
+        "s3://peregrine-voices/oliver_narrative2_parrot_saad/manifest.json",
+      defaultVoiceEngine: "PlayHT2.0",
+    });
+
+    const generated = await PlayHT.generate(userInput, {
+      voiceEngine: "PlayHT2.0",
+      voiceId:
+        "s3://peregrine-voices/oliver_narrative2_parrot_saad/manifest.json",
+      outputFormat: "mp3",
+      temperature: 1.5,
+      quality: "high",
+      speed: 0.8,
+    });
+
+    // Grab the generated file URL
+    const { audioUrl } = generated;
+
+    console.log("The url for the audio file is", audioUrl);
+    res.status(200).send({ audiourl: audioUrl, userInput: userInput });
+  } catch (error) {
+    console.error("Error generating speech:", error.message);
+    res.status(500).json({ error: "Error generating speech" });
+  }
+});
+module.exports = router;

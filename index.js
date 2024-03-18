d1967015f3ac3e53d3d39bea55f28fa83da3d802
ashrafali2000 <ali4282271@gmail.com>
2024-03-18 14:09:49 +0500

    add bodyparser

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 const port = process.env.PORT || 8000;
 
 app.use("/speech", speechRoutes);
